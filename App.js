aa66c86c4dda4b8dcc9fb9fac166349e1d37054f
Matias Godoy <matigodoy93@hotmail.com>
2020-07-11 19:04:42 -0300

    HTML y CSS: Listos

--- App.js
+++ App.js
@@ -10,58 +10,96 @@ class App extends React.Component {
 {/* -------------------------- MENU -------------------------- */}
                 
                 <header>
-                    <div className="titulo-fecha">
-                        <h1>Hoteles</h1>
-                        <h4>desde el martes, 1 de enero de 2019 hasta el miercoles, 2 de enero de 2019</h4>
-                    </div>
 
-                    <div className="inputs-hoteles">
+                    <div className="contenidoHeader">
+                        <div className="titulo-fecha">
+                            <h1 className="header-titulo">Hoteles</h1>
+                            <h4 className="info-header">desde el martes, 1 de enero de 2019 hasta el miercoles, 2 de enero de 2019</h4>
+                        </div>
 
-                    <input type="date" name="fecha1" id="" />
-                    <input type="date" name="fecha2" id="" />
-                    <select name="paises" id="">
-                        <option value="argentina">Argentina</option>
-                        <option value="colombia">Colombia</option>
-                        <option value="brasil">Brasil</option>
-                    </select>
+                        <div className="inputs-hoteles">
+                            <div className="contenido-inputs">
+                                <div className="inputDeLaFecha">
+                                    <i className="fas fa-sign-in-alt tag"></i>
+                                    <input className="input input-fecha" type="date" name="fecha1" id="" />
+                                </div>
 
-                    <select name="" id="">
-                        <option value="Barato">Barato</option>
-                        <option value="Caro">Caro</option>
-                        <option value="carisimo">Carisimo</option>
-                    </select>
+                                <div className="inputDeLaFecha">
+                                    <i className="fas fa-sign-in-alt tag"></i>
+                                    <input className="input input-fecha" type="date" name="fecha2" id="" />
+                                </div>
 
-                    <select name="tamanio" id="">
-                        <option value="pequenio">Pequeño</option>
-                        <option value="mediano">Mediano</option>
-                        <option value="grande">Grande</option>
-                    </select>
-                    
+                                <div className="contenedorTagInput">
+                                    <i className="fas fa-globe tag"></i>
+                                    <select className="input input-select " name="paises" id="">
+                                        <option value="argentina">Argentina</option>
+                                        <option value="colombia">Colombia</option>
+                                        <option value="brasil">Brasil</option>
+                                    </select>
+                                </div>
+
+                                <div className="contenedorTagInput">
+                                    <i className="fas fa-dollar-sign tag"></i>
+                                    <select className="input input-select" name="" id="">
+                                        <option value="Barato">Barato</option>
+                                        <option value="Caro">Caro</option>
+                                        <option value="carisimo">Carisimo</option>
+                                    </select>
+                                </div>
+
+                                <div className="contenedorTagInput">
+                                    <i className="fas fa-bed tag"></i>
+                                    <select className="input input-select" name="tamanio" id="">
+                                        <option value="pequenio">Pequeño</option>
+                                        <option value="mediano">Mediano</option>
+                                        <option value="grande">Grande</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </header>
 
 {/* -------------------------- MAINVIEW -------------------------- */}
 
-                <div className="mainView">
+                <main className="mainView">
                     <div className="todasLasTarjetas">
+
                         <div className="tarjeta">
-                            <div className="imagen-hotel">
-                                <img src="./images/la-bamba-de-areco.jpg" alt="Nombre del hotel" />
-                            </div>
+                                <img className="imagenHotel" src="./images/la-bamba-de-areco.jpg" alt="Nombre del hotel" />
                             <div className="info">
                                 <h1 className="titulo-hotel">La Bamba de Areco</h1>
                                 <p className="descripcion-hotel">La Bamba de Areco está ubicada en San Antonio de Areco, en el corazón de la pampa. Es una de las estancias m'as antiguas de la Argentina, recientemente restaurada para ofrecer a sus huespedes todo el confort y esplendor colonial.</p>
                                 <div className="lugar-hab-precio">
-                                    <p className="">Buenos Aires, Argentina</p>
+                                    <div className="bg-pais">
+                                        <div className="tagCaja">
+                                            <i className="fas fa-map-marker tagMain"></i>
+                                        </div>
+                                        <p className="pais">Buenos Aires, Argentina</p>
+                                    </div>
                                     <div className="habitaciones-precio">
-                                        <p className="habitaciones">11 Habitaciones</p>
-                                        <p className="precio">$$$$</p>
+                                        <div className="bg-habitaciones">
+                                            <div className="tagCaja">
+                                                <i className="fas fa-bed tagMain"></i>
+                                            </div>
+                                            <p className="habitaciones">11 Habitaciones</p>
+                                        </div>
+                                        <div className="bg-precio">
+                                            <div className="tagCaja">
+                                                <i className="fas fa-dollar-sign tagMain"></i>
+                                                <i className="fas fa-dollar-sign tagMain"></i>
+                                                <i className="fas fa-dollar-sign tagMain"></i>
+                                                <i className="fas fa-dollar-sign tagMain"></i>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <input className="btn-reserva" type="submit" value="Reservar" name="reserva" id="" />
                         </div>
+
                     </div>
-                </div>
+                </main>
 
 
             </div>
